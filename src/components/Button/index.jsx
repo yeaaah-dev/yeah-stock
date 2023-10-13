e6a2571f670b594e40ed55f6b79c21290cdd69ae
Yan-pg <yancesa.07@gmail.com>
2023-10-13 12:22:50 -0300

    initial commit

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,7 @@
+export function Button() {
+    return (
+        <div>
+            Button Component
+        </div>
+    )
+}
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,17 @@
+import { useEffect } from "react"
+import axios from "axios"
+import './style.css'
+
+function App() {
+  useEffect(() => {
+    axios.get('http://localhost:3000/products').then((response)=>{
+      console.log(response)
+    })
+  }, [])
+
+  return (
+    <div className="container-home">Yan Gostoso</div>
+  )
+}
+
+export default App
