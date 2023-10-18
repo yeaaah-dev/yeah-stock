d60501891f02bf3b4ba0e9363a8352323d5ad28b
isacmoreira10e <isacmoreira547@gmail.com>
2023-10-18 16:01:54 -0300

    back end request

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,16 +1,30 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import axios from "axios"
 import './style.css'
 
+
 function App() {
+  const [products, setProducts] = useState([])
+
   useEffect(() => {
-    axios.get('http://localhost:3000/products').then((response) => {
-      console.log(response)
-    })
+    axios.get('http://localhost:3000/products')
+      .then((response) => {
+        setProducts(response.data)
+      })
   }, [])
 
   return (
-    <div className="container-home">Yan Gostoso</div>
+    <div className="container-home">{products.map(product => {
+      return <div key={product.key} >
+        <span className=""> title: {product.title}</span>
+        <span className="">quantify: {product.quantify}</span>
+        <span className="">measure in: {product.measurein}</span>
+        <span className="">purchase price:  {product.purchasePrice}</span>
+        <span className="">sale price:  {product.salePrice}</span>
+      </div>
+    })}
+    </div>
+
   )
 }
 
