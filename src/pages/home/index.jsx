c5346b081bfe7e8396d463527a12f59ca7ec438f
Yan-pg <yancesa.07@gmail.com>
2023-10-25 14:06:42 -0300

    feat: implements search

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,62 +1,63 @@
-import { useEffect, useState } from "react"
-import axios from "axios"
-import Modal from "../../components/ModalComponent/Modal"
-import { Tab } from "../../components/Tab/Tab"
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Modal from "../../components/ModalComponent/Modal";
+import { Tab } from "../../components/Tab/Tab";
+import { Header } from "../../components/header/header";
+import { Input } from "../../components/Input";
 
 const tabs = [
   {
-    title: 'tipo 1',
-    key: 0
+    title: "tipo 1",
+    key: 0,
   },
   {
-    title: 'tipo 2',
-    key: 1
+    title: "tipo 2",
+    key: 1,
   },
   {
-    title: 'tipo 3',
-    key: 2
-  }
-]
+    title: "tipo 3",
+    key: 2,
+  },
+];
 
 function App() {
-  const [products, setProducts] = useState([])
-  const [currentTab, setCurrentTab] = useState(1)
-
+  const [products, setProducts] = useState([]);
+  const [currentTab, setCurrentTab] = useState(1);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
-    axios.get('http://localhost:3000/products')
-      .then((response) => {
-        setProducts(response.data)
-      })
-  }, [])
+    const query = search.length ? `?title_like=${search}` : "";
+
+    axios.get(`http://localhost:3000/products${query}`).then((response) => {
+      setProducts(response.data);
+    });
+  }, [search]);
 
   return (
     <div className="container-home">
+      <Input
+        onChange={(event) => {
+          setSearch(event.currentTarget.value);
+        }}
+      />
 
-      <div>  {products.map(product => {
-        return (
-          <div key={product.key} >
-            <span className=""> title: {product.title}</span>
-            <span className="">quantify: {product.quantify}</span>
-            <span className="">measure in: {product.measurein}</span>
-            <span className="">purchase price:  {product.purchasePrice}</span>
-            <span className="">sale price:  {product.salePrice}</span>
-          </div>
-        )
-      })}
+      <div>
+        {products.map((product) => {
+          return (
+            <div key={product.key}>
+              <span className=""> title: {product.title}</span>
+              <span className="">quantify: {product.quantify}</span>
+              <span className="">measure in: {product.measurein}</span>
+              <span className="">purchase price: {product.purchasePrice}</span>
+              <span className="">sale price: {product.salePrice}</span>
+            </div>
+          );
+        })}
       </div>
 
-      <Tab
-        tabs={tabs}
-        currentTab={currentTab}
-        onChange={(key) => setCurrentTab(key)}
-      />
-
-      <Modal
-        name="Yan Cesar"
-      />
-    </div >
-  )
+      <Modal name="Yan Cesar" />
+    </div>
+  );
 }
 
-export default App
+export default App;
