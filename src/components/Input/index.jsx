6dc411a9b6127d10c925866fdad8992319ed675a
Yan-pg <yancesa.07@gmail.com>
2023-10-25 14:06:25 -0300

    feat: add input component

---
+++ src/components/Input/index.jsx
@@ -0,0 +1,19 @@
+import { MagnifyingGlass } from "phosphor-react";
+import styles from "./styles.module.css";
+
+export function Input({ ...rest }) {
+  return (
+    <div className={styles["input-wrapper"]}>
+      <button className={styles["button-search"]}>
+        <MagnifyingGlass size={16} />
+      </button>
+
+      <input
+        {...rest}
+        type="text"
+        className={styles["input-header"]}
+        placeholder="Pesquisar produto"
+      />
+    </div>
+  );
+}
