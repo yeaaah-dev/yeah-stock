2cc8bb4fade43031ed1eb02dfb681fc16e37748b
isacmoreira10e <isacmoreira547@gmail.com>
2023-10-26 09:09:29 -0300

    feat: Syntax errors resolved

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,16 +1,14 @@
+/* eslint-disable react/prop-types */
 import styles from '../Button/Button.module.css'
 
 export function Button(
-    {label, buttonBackgroundOff, icon: Icon}
+    { label, buttonBackgroundOff, icon: icon }
 ) {
     return (
         <div>
-            <button className={buttonBackgroundOff === 'yes' ? `${styles['normal_button']} ${styles['background_off']}` : styles['normal_button'] }>
-
-                <Icon /> 
-                
+            <button className={buttonBackgroundOff === 'yes' ? `${styles['normal_button']} ${styles['background_off']}` : styles['normal_button']}>
+                {icon}
                 {label}
-
             </button>
         </div>
     )
