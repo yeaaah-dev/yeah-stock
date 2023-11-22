e24352198814f19332347e56261e5cc12d749045
isacmoreira10 <isacmoreira547@gmail.com>
2023-11-22 10:47:31 -0300

    refactrure: refactor styles

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -10,7 +10,7 @@ import { Card } from "../../components/Card";
 import { Button } from "../../components/Button/Button";
 import style from "../home/app.module.css";
 import Rick from "../../assets/images/RickAndMory.png";
-import { modalStatus } from "../../components/ModalComponent/Modal";
+// import { modalStatus } from "../../components/ModalComponent/Modal";
 import { useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -39,7 +39,7 @@ export function App() {
   const [search, setSearch] = useState("");
   const [products, setProducts] = useState([]);
   const [layout, setLayout] = useState(iconType.COLUMNS);
-  const [modalModel, setModalModel] = useState(modalStatus.CLOSE);
+  // const [modalModel, setModalModel] = useState(modalStatus.CLOSE);
   const [productSelected, setProductSelected] = useState({});
   const navigate = useNavigate();
 
@@ -77,37 +77,37 @@ export function App() {
   }
 
   function onChangeModalStatusClose() {
-    setModalModel(modalStatus.CLOSE);
+    // setModalModel(modalStatus.CLOSE);
   }
 
   function onChangeModalStatusOpen(product) {
     setProductSelected(product);
-    setModalModel(modalStatus.OPEN);
+    // setModalModel(modalStatus.OPEN);
   }
 
-  function changeLayoutCards() {
-    if (layout === iconType.LIST && modalModel === modalStatus.CLOSE) {
-      return style["grid-layout-list-close-modal"];
-    } else if (layout === iconType.LIST && modalModel === modalStatus.OPEN) {
-      return style["grid-layout-list-open-modal"];
-    }
-    if (modalModel === modalStatus.OPEN && layout === iconType.COLUMNS) {
-      return style["grid-layout-column-open-modal"];
-    } else if (modalModel === modalStatus.CLOSE) {
-      return style["grid-layout-column-close-modal"];
-    }
-  }
+  // function changeLayoutCards() {
+  //   //   if (layout === iconType.LIST && modalModel === modalStatus.CLOSE) {
+  //   //     return style["grid-layout-list-close-modal"];
+  //   //   } else if (layout === iconType.LIST && modalModel === modalStatus.OPEN) {
+  //   //     return style["grid-layout-list-open-modal"];
+  //   //   }
+  //   //   if (modalModel === modalStatus.OPEN && layout === iconType.COLUMNS) {
+  //   //     return style["grid-layout-column-open-modal"];
+  //   //   } else if (modalModel === modalStatus.CLOSE) {
+  //   //     return style["grid-layout-column-close-modal"];
+  //   //   }
+  // }
 
-  function changeLayoutAllScreen() {
-    if (layout === iconType.LIST && modalModel === modalStatus.OPEN) {
-      return style["main-width-close-modal"];
-    }
-    if (modalModel === modalStatus.OPEN) {
-      return style["main-width-open-modal"];
-    } else {
-      return style["main-width-close-modal"];
-    }
-  }
+  // function changeLayoutAllScreen() {
+  //   // if (layout === iconType.LIST && modalModel === modalStatus.OPEN) {
+  //   //   return style["main-width-close-modal"];
+  //   // }
+  //   // if (modalModel === modalStatus.OPEN) {
+  //   //   return style["main-width-open-modal"];
+  //   // } else {
+  //   //   return style["main-width-close-modal"];
+  //   // }
+  // }
 
   async function getProducts() {
     const { error } = toastInstance("The product has not been deleted !");
@@ -147,113 +147,61 @@ export function App() {
   }, [search]);
 
   return (
-    <div className={style["home-page"]}>
-      <aside>
-        <Sidebar />
-      </aside>
+    <>
+      <Sidebar />
 
-      <main className={changeLayoutAllScreen()}>
-        <nav className={style["nav-style"]}>
-          <div className={style["content-input"]}>
-            <div className={style["input-wrapper"]}>
-              <button
-                className={style["button-search"]}
-                onClick={goToRegistration}
-              >
-                <MagnifyingGlass size={16} />
-              </button>
-              <Input
-                borderNone={true}
-                type="text"
-                placeholder="Search products"
-                onChange={(event) => {
-                  setSearch(event.currentTarget.value);
-                }}
-              />
-            </div>
-          </div>
-          <div className={style["button-icons"]}>
-            <div className={style["div-button"]}>
-              <Button
-                label={
-                  <span className={style["name-button"]}>Add product</span>
-                }
-                icon={<Icon className={style["icon-plus"]} />}
-                buttonBackgroundOff={"not"}
-                className={style["Button"]}
-                onClick={goToRegistration}
-              />
-            </div>
+      <button className={style["button-search"]} onClick={goToRegistration}>
+        <MagnifyingGlass size={16} />
+      </button>
+      <Input
+        borderNone={true}
+        type="text"
+        placeholder="Search products"
+        onChange={(event) => {
+          setSearch(event.currentTarget.value);
+        }}
+      />
 
-            <div className={style["icon-photo"]}>
-              <Bell size={20} className={style["icon-bell"]} />
-            </div>
+      <Button
+        label={<span className={style["name-button"]}>Add product</span>}
+        icon={<Icon className={style["icon-plus"]} />}
+        buttonBackgroundOff={"not"}
+        className={style["Button"]}
+        onClick={goToRegistration}
+      />
 
-            <div className={style["notification"]}>
-              <span className={style["number-notification"]}>13</span>
-            </div>
+      <Bell size={20} className={style["icon-bell"]} />
+      <span className={style["number-notification"]}>13</span>
+      <img src={Rick} className={style["logo"]} alt="Rick and Morty image" />
 
-            <div>
-              <img
-                src={Rick}
-                className={style["logo"]}
-                alt="Rick and Morty image"
-              />
-            </div>
-          </div>
-        </nav>
-
-        <section>
-          <p className={style["title"]}>Start</p>
-
-          <div className={style["tab-toggle"]}>
-            <div>
-              <Tab
-                tabs={tabs}
-                currentTab={currentTab}
-                onChange={(layout) => setCurrentTab(layout)}
-              />
-            </div>
-
-            <div>
-              <Toggle onChange={(layout) => setLayout(layout)} />
-            </div>
-          </div>
-
-          <div className={changeLayoutCards()}>
-            {products.map((product) => {
-              return (
-                <Card
-                  key={product.id}
-                  product={product}
-                  layout={layout}
-                  onChangeModalStatusOpen={(product) =>
-                    onChangeModalStatusOpen(product)
-                  }
-                />
-              );
-            })}
-          </div>
-        </section>
-      </main>
-
-      <section
-        className={
-          modalModel === modalStatus.CLOSE
-            ? style["modal-close"]
-            : style["modal-section"]
-        }
-      >
-        <Modal
-          name="Yan Cesar"
-          onChangeModalStatusClose={onChangeModalStatusClose}
-          goToEdition={goToEdition}
-          product={productSelected}
-          onDeleteProduct={deleteProduct}
-        />
-      </section>
+      <p className={style["title"]}>Start</p>
+      <Tab
+        tabs={tabs}
+        currentTab={currentTab}
+        onChange={(layout) => setCurrentTab(layout)}
+      />
+      <Toggle onChange={(layout) => setLayout(layout)} />
+      {products.map((product) => {
+        return (
+          <Card
+            key={product.id}
+            product={product}
+            layout={layout}
+            onChangeModalStatusOpen={(product) =>
+              onChangeModalStatusOpen(product)
+            }
+          />
+        );
+      })}
+      <Modal
+        name="Yan Cesar"
+        onChangeModalStatusClose={onChangeModalStatusClose}
+        goToEdition={goToEdition}
+        product={productSelected}
+        onDeleteProduct={deleteProduct}
+      />
       <ToastContainer />
-    </div>
+    </>
   );
 }
 
